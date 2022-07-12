08e95504c28a6db02242c3fb014a87d5bc9fc377
Tasos Antoniou <tassant@hotmail.com>
2022-07-12 17:28:00 +0300

    S5L27 NOT FINISHED

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css"
+    integrity="sha384-zIaWifL2YFF1qaDiAo0JFgsmasocJ/rqu7LKYH8CoBEXqGbb9eO+Xi3s6fQhgFWM" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>css/style.css">
     <title><?php echo SITENAME; ?></title>
 </head>
 <body>
-    
+<div class="container"><?php require_once APPROOT.'/views/inc/navbar.php';?>    
