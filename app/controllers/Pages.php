8d6972cf6a74f78bd7b0d463ee1d87d2ed33e891
Tasos Antoniou <tassant@hotmail.com>
2022-07-12 14:11:55 +0300

    InitCommit

---
+++ app/controllers/Pages.php
@@ -0,0 +1,27 @@
+<?php
+    class Pages extends Controller {
+        public function __construct(){
+          // $this->postModel = $this->model('Post');;
+        }
+        
+        public function index(){
+          //  $posts = $this->postModel->getPosts();
+            //$this->view('hello'); //must die bec view.php does not exist
+            //$this->view('pages/index', ['title' => 'Welcome']); // array ['title'(key) => 'Welcome' (value)]
+            $data = [
+                'title'=> 'Welcome'//,
+                //'posts'=> $posts
+            ];
+            
+            
+
+            $this->view('pages/index', $data);
+        }
+
+        public function about(){
+            $data = [
+                'title'=> 'About Us'
+            ];
+            $this->view('pages/about', $data);
+        }
+    }
