5c7ba6bbeeda0890942fc6948041e3915f1419dc
Tasos Antoniou <tassant@hotmail.com>
2022-07-15 23:09:53 +0300

    S5L27_6 Min 03 Sec

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -4,8 +4,9 @@ class Navbar{
 }
 ?>
 
-<nav class="navbar navbar-expand-lg bg-light rounded" aria-label="Twelfth navbar example">
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-3 " aria-label="Twelfth navbar example">
       <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo URLROOT;?>"><?php echo SITENAME;?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -13,14 +14,16 @@ class Navbar{
         <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Centered nav only</a>
+              <a class="nav-link active" aria-current="page" href="<?php echo URLROOT;?>">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+              <a class="nav-link" href="<?php echo URLROOT;?>pages/about">About</a>
             </li>
             <li class="nav-item">
               <a class="nav-link disabled">Disabled</a>
             </li>
+            </ul>
+          <ul class="navbar-nav ml-auto">
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="dropdown10" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
               <ul class="dropdown-menu" aria-labelledby="dropdown10">
