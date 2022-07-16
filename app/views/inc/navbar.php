a724b878390edf608356d9779d245aa46d119ad9
Tasos Antoniou <tassant@hotmail.com>
2022-07-16 15:25:46 +0300

    Bug with horizontal-align navbar

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -1,14 +1,14 @@
-<nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-3 " aria-label="Twelfth navbar example">
-      <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-3 ">
+      
         <a class="navbar-brand" href="<?php echo URLROOT;?>"><?php echo SITENAME;?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="<?php echo URLROOT;?>">Home</a>
+        <div class="collapse navbar-collapse" id="navbarsExample10">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="<?php echo URLROOT;?>">Home</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="<?php echo URLROOT;?>pages/about">About</a>
@@ -18,7 +18,7 @@
             </li>
             </ul>
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
+            <li class="nav-item active">
               <a class="nav-link" href="<?php echo URLROOT;?>/users/register">Register</a>
             </li>
             <li class="nav-item">
@@ -29,5 +29,5 @@
             </li>
           </ul>
         </div>
-      </div>
+      
     </nav>
