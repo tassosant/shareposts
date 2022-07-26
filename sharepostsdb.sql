7b5900b1a000890555fdd1bcc592cde3a7204326
Tasos Antoniou <tassant@hotmail.com>
2022-07-26 14:19:13 +0300

    S6L36 Created the posts controller and the index view with updated db

--- sharepostsdb.sql
+++ sharepostsdb.sql
@@ -29,7 +29,7 @@ CREATE TABLE `posts` (
   `body` text,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,6 +38,7 @@ CREATE TABLE `posts` (
 
 LOCK TABLES `posts` WRITE;
 /*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,7,'Post One','This is a test for post one','2022-07-26 10:44:51'),(2,7,'Post Two','This is a test for post two','2022-07-26 10:44:51');
 /*!40000 ALTER TABLE `posts` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -77,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-07-26 10:39:05
+-- Dump completed on 2022-07-26 14:18:49
